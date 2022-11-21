6653789d6bfe029b695cdae9724d84d498bbb7f0
Marker97 <chereche10@gmail.com>
2022-11-21 17:39:41 +0100

    ComparisonBetweenTypes

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    val number1 = "222"
+    val number2 = 333
+    println("Number1 is bigger than Number2? ${number1.toLong() > number2}")
+    println("Number1 is smaller than Number2? ${number1.toInt() < number2}")
+
+}
